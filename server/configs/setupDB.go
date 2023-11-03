9a61436b0d16fd8dc06ad6146565d77809403c2d
MeabhG97 <91346624+MeabhG97@users.noreply.github.com>
2023-11-03 11:11:54 +0000

    Connected DB to Server

---
+++ server/configs/setupDB.go
@@ -0,0 +1,32 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	clientOptions := options.Client().ApplyURI(GetMongoURI())
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to DB")
+
+	return client
+}
