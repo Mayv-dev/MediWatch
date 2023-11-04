dc40f1bbee88c37bf1ac95ef5307dc7207f53ca7
MeabhG97 <91346624+MeabhG97@users.noreply.github.com>
2023-11-04 21:38:36 +0000

    Setup user controller

---
+++ server/controllers/UserController.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"context"
+	"mediwatch/server/configs"
+	"mediwatch/server/models"
+	"mediwatch/server/views"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "Users")
+
+func GetAllUsers(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var users []models.User
+
+	results, err := userCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, views.UserView{Status: http.StatusInternalServerError, Message: "Error", Data: map[string]interface{}{"data": err.Error()}})
+		return
+	}
+
+	defer results.Close(ctx)
+
+	for results.Next(ctx) {
+		var user models.User
+
+		if err = results.Decode(&user); err != nil {
+			c.JSON(http.StatusInternalServerError, views.UserView{Status: http.StatusInternalServerError, Message: "Error", Data: map[string]interface{}{"data": err.Error()}})
+		}
+
+		users = append(users, user)
+	}
+
+	c.JSON(http.StatusOK, views.UserView{Status: http.StatusOK, Message: "Success", Data: map[string]interface{}{"data": users}})
+}
