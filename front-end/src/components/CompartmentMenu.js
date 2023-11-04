1b81eaf92d4242d242633adb2451642a2ae8359b
Jason Njoku <JasNjoku@gmail.com>
2023-11-04 13:12:25 +0000

    Create compartment menu component

---
+++ front-end/src/components/CompartmentMenu.js
@@ -0,0 +1,11 @@
+
+
+function CompartmentMenu(props) {
+    return (
+        <div id="compartment-menu">
+            <p>Compartment {props.compartment}</p>
+        </div>
+    )
+}
+
+export default CompartmentMenu;
