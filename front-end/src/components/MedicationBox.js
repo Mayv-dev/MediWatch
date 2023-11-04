4056fc7e38521f30f02d923f5795ffb5bb038602
Jason Njoku <JasNjoku@gmail.com>
2023-11-04 13:47:28 +0000

    Render compartment menu component

--- front-end/src/components/MedicationBox.js
+++ front-end/src/components/MedicationBox.js
@@ -1,18 +1,24 @@
 import BoxCompartment from "./BoxCompartment"
-import {useState} from 'react'
+import { useState } from 'react'
+import CompartmentMenu from "./CompartmentMenu"
 
 function MedicationBox(props) {
-    const [chosenBox, setChosenBox] = useState("")
+    const [chosenBox, setChosenBox] = useState()
 
     const handleSelection = (compartmentNumber) => {
         console.log("You have chosen compartment " + compartmentNumber)
-        setChosenBox(compartmentNumber)
+        const chosenCompartment = props.compartments.find(item => item.compartment == compartmentNumber)
+        setChosenBox(chosenCompartment)
     }
 
     return (
-        <div id="medication-box">
-            {props.compartments.map(item => 
-            <BoxCompartment compartmentInfo={item} colour={chosenBox === item.compartment} selected={handleSelection} key={item.compartment}/>)}
+        <div id="medication-box-container">
+            <div id="medication-box">
+                {props.compartments.map(item =>
+                    <BoxCompartment compartmentInfo={item} colour={chosenBox === item.compartment} selected={handleSelection} key={item.compartment} />)
+                }
+            </div>
+            {chosenBox !== undefined && <CompartmentMenu />}
         </div>
     )
 }
