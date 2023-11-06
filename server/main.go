8becef149d53b22ca1a7ad0c7b24c63fa586b4d8
MeabhG97 <91346624+MeabhG97@users.noreply.github.com>
2023-11-06 12:55:18 +0000

    Added Cors support

--- server/main.go
+++ server/main.go
@@ -4,11 +4,17 @@ import (
 	"mediwatch/server/configs"
 	"mediwatch/server/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:3000"}
+
+	router.Use(cors.New(corsConfig))
 
 	configs.ConnectDB()
 
