2f1f3fc2d44ea22c410dae8dc94a2386cf66fe49
MeabhG97 <91346624+MeabhG97@users.noreply.github.com>
2023-11-12 20:21:17 +0000

    Added Create User Route
    Added a route post /user and the controller functionality to
    handle the creation of a new user in the database.

--- server/controllers/UserController.go
+++ server/controllers/UserController.go
@@ -9,11 +9,14 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "Users")
+var validate = validator.New()
 
 func GetAllUsers(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -34,11 +37,42 @@ func GetAllUsers(c *gin.Context) {
 		var user models.User
 
 		if err = results.Decode(&user); err != nil {
-			c.JSON(http.StatusInternalServerError, views.UserView{Status: http.StatusInternalServerError, Message: "Error", Data: map[string]interface{}{"data": err.Error()}})
+			c.JSON(http.StatusInternalServerError, views.UserView{Status: http.StatusInternalServerError, Message: "Error", Data: err.Error()})
 		}
 
 		users = append(users, user)
 	}
 
 	c.JSON(http.StatusOK, views.UserView{Status: http.StatusOK, Message: "Success", Data: users})
+}
+
+func CreateUser(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var user models.User
+
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, views.UserView{Status: http.StatusBadRequest, Message: "Error", Data: err.Error()})
+		return
+	}
+
+	if validationErr := validate.Struct(&user); validationErr != nil {
+		c.JSON(http.StatusBadRequest, views.UserView{Status: http.StatusBadRequest, Message: "Error", Data: validationErr.Error()})
+		return
+	}
+
+	newUser := models.User{
+		Id:       primitive.NewObjectID(),
+		Email:    user.Email,
+		Password: user.Password,
+	}
+
+	result, err := userCollection.InsertOne(ctx, newUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, views.UserView{Status: http.StatusInternalServerError, Message: "Error", Data: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, views.UserView{Status: http.StatusCreated, Message: "success", Data: result})
 }
