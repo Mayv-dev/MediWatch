694dc5c50cfbb6026ae9740ca3362bc362113c24
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2023-11-18 16:17:39 +0000

    Times are now displayed in HH:MM format

--- front-end/src/components/CompartmentMenu.js
+++ front-end/src/components/CompartmentMenu.js
@@ -1,7 +1,18 @@
 function CompartmentMenu(props) {
     let times = []
-    
+    let date, hours, minutes
     props.schedule.map(scheduledDose => scheduledDose.compartment == props.selectedCompartment ? times.push(scheduledDose.datetime) : null)
+    times = times.map(time => {
+        date = new Date(time)
+        hours = date.getHours()
+        if (hours < 10) hours = `0${hours}`
+        minutes = date.getMinutes()
+        if (minutes < 10) minutes = `0${minutes}`
+        return hours + ":" + minutes
+    })
+    console.log(times)
+    
+    
     return (
         <div id="compartment-menu" className="bg-white h-40 w-10/12">
             <div id="time-column" class="bg-gray-200 w-60 mx-auto">
