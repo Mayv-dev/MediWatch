40eeca25256425b598d7ae5fcef2b2d245651664
MeabhG97 <91346624+MeabhG97@users.noreply.github.com>
2023-11-20 20:49:13 +0000

    Added Create User Schedule entry
    Added handling to create a user schedule entry.
    Added route PUT /user/:id/schedule

--- server/controllers/UserScheduleController.go
+++ server/controllers/UserScheduleController.go
@@ -42,3 +42,50 @@ func GetAllUserSchedule(c *gin.Context) {
 
 	c.JSON(http.StatusOK, views.UserView{Status: http.StatusOK, Message: "success", Data: scheduleList})
 }
+
+func CreateUserSchedule(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	id := c.Param("id")
+	objId, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, views.UserView{Status: http.StatusBadRequest, Message: "Error ID", Data: err.Error()})
+		return
+	}
+
+	var user models.User
+	var schedule models.Schedule
+
+	if err = c.BindJSON(&schedule); err != nil {
+		c.JSON(http.StatusBadRequest, views.UserView{Status: http.StatusBadRequest, Message: "Error Bind", Data: err.Error()})
+		return
+	}
+
+	if err := validate.Struct(&schedule); err != nil {
+		c.JSON(http.StatusBadRequest, views.UserView{Status: http.StatusBadRequest, Message: "Error Valid", Data: err.Error()})
+		return
+	}
+
+	update := bson.M{"id": primitive.NewObjectID(), "datetime": schedule.DateTime, "medications": schedule.Medications}
+
+	result, err := userCollection.UpdateOne(ctx, bson.M{"id": objId}, bson.M{"$push": bson.M{"schedule": update}})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, views.UserView{Status: http.StatusInternalServerError, Message: "Error", Data: err.Error()})
+		return
+	}
+
+	if result.MatchedCount == 1 {
+		err := userCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&user)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, views.UserView{Status: http.StatusInternalServerError, Message: "Error", Data: err.Error()})
+			return
+		}
+	}
+
+	scheduleList := user.Schedule
+
+	c.JSON(http.StatusOK, views.UserView{Status: http.StatusOK, Message: "success", Data: scheduleList})
+}
--- server/routes/UserScheduleRoute.go
+++ server/routes/UserScheduleRoute.go
@@ -8,4 +8,5 @@ import (
 
 func UserScheduleRouter(router *gin.Engine) {
 	router.GET("/user/:id/schedule", controllers.GetAllUserSchedule)
+	router.PUT("/user/:id/schedule", controllers.CreateUserSchedule)
 }
