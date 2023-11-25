a8fa0ba350c30cfee7e3d28c61f01ed8d4f53d3a
Jason Njoku <JasNjoku@gmail.com>
2023-11-25 17:54:05 +0000

    Create method to handle deletion of dose

--- front-end/src/components/ScheduleList.js
+++ front-end/src/components/ScheduleList.js
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react"
+import {SERVER_HOST} from "../config/global_constants";
 
 function ScheduleList(props) { 
-    console.log(props.schedule)
     let first = true;
     const monthStrings = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-    let [dateConversion,setDateConversion] = useState(props.schedule.map(item => new Date(item.datetime)))
-
+    let [dateConversion,setDateConversion] = useState({id: props.schedule.map(item => item.id), datetime: props.schedule.map(item => new Date(item.datetime))})
+    
     const handleDateConvert = () => {
         let minutes, hours, day, month;
+
         if(props.schedule != undefined) {
-            dateConversion = dateConversion.map(date => {
+            dateConversion =  dateConversion.datetime.map((date, index) => {
                 if (date.getDate() % 10 != 1) {
                     if (date.getDate() % 10 != 2) {
                         if (date.getDate() % 10 == 3) day = date.getDate() + "rd"
@@ -22,18 +23,31 @@ function ScheduleList(props) {
                 date.getMinutes() / 10 < 1 ? minutes = "0" + date.getMinutes() : minutes = date.getMinutes()
                 month = monthStrings[date.getMonth()]
 
-                return day + " " + month + " " + date.getFullYear() + " - " + hours + ":" + minutes
+                return {id: dateConversion.id[index], datetime: day + " " + month + " " + date.getFullYear() + " - " + hours + ":" + minutes}
             })
         }
     }
 
     useEffect(() => {
-        setDateConversion(props.schedule.map(item => new Date(item.datetime)))
+        setDateConversion({id: props.schedule.map(item => item.id), datetime: props.schedule.map(item => new Date(item.datetime))})
     },[])
     handleDateConvert()
     
     let [list,setList] = useState(dateConversion)
+  
     
+    const deleteDose = (e) => {
+        const sId = e.currentTarget.id
+
+        fetch(`${SERVER_HOST}/user/${props.userID}/schedule/${sId}`, {
+            method: "DELETE",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+
     return (
         <>
         <div className="flex flex-col w-1/2 h-3/4">
@@ -41,7 +55,11 @@ function ScheduleList(props) {
             <div className="border p-1 h-full border-black rounded-br rounded-bl w-3/4 m-auto bg-white  ">
                 <ul className="h-full overflow-y-scroll">
                     {list == undefined ? <h1>No schedule has been set</h1>:
-                    list.map(listItem => <li className="border-2 m-1 bg-gray-100">{listItem}</li>)}
+                    list.map(listItem => <li className="border-2 m-1 bg-gray-100 flex justify-around items-center">
+                        {listItem.datetime}
+                        <button id={listItem.id}  onClick={deleteDose} className="bg-red-600 rounded-md p-1 hover:bg-red-700">Delete Dose</button>
+                    </li>)
+                    }                    
                 </ul>
             </div>
         </div>
