8aad20abf8f4c93ee55f6921479f439072f5d650
Cian Ashby <cianashby1337@gmail.com>
2023-12-17 23:48:26 +0000

    Play sound when dose must be taken

--- front-end/src/components/BoxCompartment.js
+++ front-end/src/components/BoxCompartment.js
@@ -1,11 +1,22 @@
+// use-sound code was taken from https://www.npmjs.com/package/use-sound
+import { useSound } from 'use-sound';
+import {useEffect} from 'react'
+import alert from '../sounds/beep-06.mp3';
+
 function BoxCompartment(props) {
-    let selected = Math.abs(Date.parse(props.item.datetime)-Date.now()) < 1800000 ? "compartmentDiv bg-green-500" : props.colour ?  "compartmentDivSelected" : "compartmentDiv"
+    let selected = Math.abs(Date.parse(props.item.datetime)-Date.now()) < 1800000 ? "compartmentDiv bg-green-500" : props.colour ?  "compartmentDivSelected" : "compartmentDiv bg-white "
+    
+    const [play] = useSound(alert);
+
+    useEffect(() => {
+        if(Math.abs(Date.parse(props.item.datetime)-Date.now()) < 1800000) play()
+    })
+    
     return (
-        <div className={selected + " bg-white h-20 w-20"} onClick={() => props.onSelection(props.identifier)}>
-            
-    {Math.abs(Date.parse(props.item.datetime)-Date.now()) < 3600000 ? 
-        Math.abs(Date.parse(props.item.datetime)-Date.now()) < 1800000 ? <p>Due Now</p> : <p>Due Soon</p>
-        :null}
+        <div className={selected + " h-20 w-20"} onClick={() => props.onSelection(props.identifier)}>
+        {Math.abs(Date.parse(props.item.datetime)-Date.now()) < 3600000 ? 
+            Math.abs(Date.parse(props.item.datetime)-Date.now()) < 1800000 ? <p>Due Now</p> : <p>Due Soon</p>
+            :null}
         </div>
     )
 }
